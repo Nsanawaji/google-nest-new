104bcd0f3bd4b42f3744645138fc0899c8c44e98
Nsanawaji <nsanawajiadasi@gmail.com>
2024-05-30 14:16:32 +0100

    Adding user resource

---
+++ src/database/database.module.ts
@@ -0,0 +1,8 @@
+import { Controller, Module } from "@nestjs/common"
+import { MongooseModule } from "@nestjs/mongoose";
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [],
+  providers: [],
+})
+export class DatabaseModule {}
