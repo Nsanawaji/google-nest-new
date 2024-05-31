e4327a1555ca3e532e00311fbd8d7e5dac2e19e9
Nsanawaji <nsanawajiadasi@gmail.com>
2024-05-31 11:36:24 +0100

    Adding signup route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from 'src/dtos/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('signup')
+  async register(@Body() payload: UserDto) {
+    await this.userService.registerUser(payload);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDto } from 'src/dtos/user.dto';
+import { User } from 'src/schemas/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async registerUser(payload: UserDto) {
+    const user = await new this.userModel(payload);
+    await user.save;
+    return user;
+  }
+}
